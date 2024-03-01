ede49d38bcdac5cfc5243d924120e3cfd850bd6c
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-03-01 11:35:33 +0200

    added a whatsapp message struct

--- WhatsAppProject/Controllers/HomeViewController/Model/WhatsappChatModelStruct.swift
+++ WhatsAppProject/Controllers/HomeViewController/Model/WhatsappChatModelStruct.swift
@@ -8,11 +8,32 @@
 import Foundation
 import UIKit
 
-struct WhatsappChatModel {
-    let senderProfileImage: UIImage
-    let senderIcon: UIImage
-    let markReadIcon: UIImage
-    let senderName: String
-    let senderLastMessage: String
-    let lastMessageTimeStamp: Date
+struct WhatsappMessage {
+    let messageId: String
+    let sender: WhatsappUser
+    let receiver : WhatsappUser
+    let timestamp: Date
+    let messageText: String
+    let attachments: [String]
+    let messageType: String // eg voice note, video, photo, documents, voice calls, video calls, gif. contact,message location
+    let messageStatus: String // eg Status of the message (sent, delivered, read)
+   
+    // let reactions: [Reaction]
+    // let referencedMessageId: String
+    // let isFowardedMessage: Bool
+    // let numberOfFwardedTimes: Int
+    // let location: Location
+   // let contactInfo: ContactInfo?
+    // let groupInfo: GroupInfo?
+    
+}
+
+struct WhatsappUser {
+    let userId : String
+    let profileImage: UIImage
+    let name : String
+    let phoneNumber : String
+    
+
+    
 }
